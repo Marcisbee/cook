b9dcf3b5045a4b2c78e018ade1e81d3c46f59d27
Marcis Bergmanis <marcisbergmanis@gmail.com>
2021-07-17 14:14:08 +0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './scenes/restaurant'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
